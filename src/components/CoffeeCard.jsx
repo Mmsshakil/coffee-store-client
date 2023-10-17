524cea0f2c0cd8a26ad99d171f90dbb1f100112d
Mmsshakil <softhandshakil@gmail.com>
2023-10-18 04:14:25 +0600

    coffee deleted done but need to work with reload problem after delete

--- src/components/CoffeeCard.jsx
+++ src/components/CoffeeCard.jsx
@@ -1,7 +1,47 @@
 
+import Swal from 'sweetalert2'
+
 const CoffeeCard = ({ coffee }) => {
 
-    const { name, quantity, supplier, taste, category, details, photo } = coffee;
+    const { _id, name, quantity, supplier, taste, category, details, photo } = coffee;
+
+
+    const handleDelete = _id => {
+        console.log(_id);
+
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to delete this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+
+
+                // console.log('Delete confirm',_id);
+
+                fetch(`http://localhost:5000/coffee/${_id}`, {
+                    method: 'DELETE'
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log(data);
+                        if (data.deletedCount > 0) {
+                            Swal.fire(
+                                'Deleted!',
+                                'Your coffee has been deleted.',
+                                'success'
+                            )
+                        }
+                    })
+
+            }
+        })
+
+    }
 
     return (
         <div className="card card-side bg-base-100 shadow-xl">
@@ -16,9 +56,13 @@ const CoffeeCard = ({ coffee }) => {
                 </div>
                 <div className="card-actions justify-end">
                     <div className="btn-group btn-group-vertical space-y-5">
-                        <button className="btn">View</button>
-                        <button className="btn">Edit</button>
-                        <button className="btn">Delete</button>
+                        <button className="btn btn-outline btn-info">View</button>
+                        <button className="btn btn-outline btn-warning">Edit</button>
+
+                        <button
+                            onClick={() => handleDelete(_id)}
+                            className="btn btn-outline btn-error">Delete</button>
+
                     </div>
                 </div>
             </div>
